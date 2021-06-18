876dc6c2a92079cb37dd7f0ba1cd0a7042911d14
zhaofuqiang <33599707+zfq314@users.noreply.github.com>
2021-06-18 17:15:50 +0800

    优化代码

--- spark3-ZhaoFuQiang/src/main/scala/WordCount.scala
+++ spark3-ZhaoFuQiang/src/main/scala/WordCount.scala
@@ -1,4 +1,3 @@
-import org.apache.spark.rdd.RDD
 import org.apache.spark.{SparkConf, SparkContext}
 
 object WordCount {
@@ -7,11 +6,18 @@ object WordCount {
 
     val sc = new SparkContext(conf)
     val path = this.getClass.getResource("/").getPath()
-    val inputSteam  = sc.textFile(path + "log4j.properties")
+    val inputSteam = sc.textFile(path + "log4j.properties")
       .flatMap(_.split("\\s"))
       .map((_, 1))
       .reduceByKey(_ + _)
+
     val result = inputSteam.collect()
+    //设置类型推断的
     result.foreach(println)
+
+    //Shut down the SparkContext.，关闭执行环境
+    //stop是停止appication的意思，spark是做垃圾回收，防止资源泄露。
+
+    sc.stop()
   }
 }
